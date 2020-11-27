bda009186dbe8c7731f5e71bbb363a20beca9a2c
gschian0 <gschiano@me.com>
2020-11-27 10:56:16 -0500

    with build folder

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -42388,7 +42388,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55564" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62934" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
